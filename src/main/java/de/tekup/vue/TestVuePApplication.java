934635cedadb7309381896e44dc7e4fad2325b3a
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2020-11-26 19:12:13 +0100

    Index page

---
+++ src/main/java/de/tekup/vue/TestVuePApplication.java
@@ -0,0 +1,13 @@
+package de.tekup.vue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestVuePApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestVuePApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/tekup/vue/TestVuePApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tekup.vue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestVuePApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
