a6f683bebdc8e260ca1ca4121b3bc83b7409a13d
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2020-12-03 20:17:47 +0100

    Update UserRestService.java

--- src/main/java/de/tekup/vue/services/UserRestService.java
+++ src/main/java/de/tekup/vue/services/UserRestService.java
@@ -7,8 +7,7 @@ import org.springframework.web.client.RestTemplate;
 
 
 import de.tekup.vue.models.dto.User;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+
 
 @Service
 public class UserRestService {
@@ -16,23 +15,16 @@ public class UserRestService {
 	public void sendUserToDB(User request) {
 		RestTemplate template = new RestTemplate();
 		try {
-			ResponseEntity<Object> response = template.postForEntity(URL, request, Object.class);
-		if (response.getStatusCodeValue()==200) {
-			System.out.println("User " +(User)response.getBody());
-		}else {
-			System.out.println("Error" + response.getBody());
-		}
+			ResponseEntity<User> response = template.postForEntity(URL, request, User.class);
+		
+			System.out.println("User " +response.getBody());
+		
 		}catch (HttpClientErrorException e) {
 			// TODO: handle exception
-			System.err.println("there is an error");
+			System.err.println("there is an error : "+ e.getResponseBodyAsString());
 		}
 		
 		
 	}
 }
 
-@Data
-@AllArgsConstructor
-class ErrorRes{
-	private String msgErrors;
-}
